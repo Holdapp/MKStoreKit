145eadea96d4251e5c180f6894bc14da715275ae
badeleux <badeleux@gmail.com>
2013-07-30 15:42:32 +0200

    alogs dlogs

--- MKStoreManager.m
+++ MKStoreManager.m
@@ -47,6 +47,15 @@
 #error "MKStoreKit uses features (NSJSONSerialization) only available in iOS SDK  and later."
 #endif
 
+#ifdef DEBUG
+#   define DLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+#else
+#   define DLog(...)
+#endif
+
+// ALog always displays output regardless of the DEBUG setting
+#define ALog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+
 
 @interface MKStoreManager () //private methods and properties
 
